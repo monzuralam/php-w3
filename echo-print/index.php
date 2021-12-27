81f3f988d1965891a6d86ddae6418686a72219b4
monzuralam <monzur707@gmail.com>
2021-12-27 22:49:23 +0600

    echo/print

---
+++ echo-print/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Echo / echo ()
+ * Print / print()
+ */
+$a = 'PHP';
+$learn = "Learn PHP";
+$school = "from W3school";
+
+echo "hello i am php"."<br>";
+echo "I love " . $a . "all time";
+echo "<br>";
+
+print "Hello i,m from print <br>";
+print $learn . " ";
+print $school;
+print("<br> Loading...");
