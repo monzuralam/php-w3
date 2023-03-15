d135dee56e9cde3547fdd921756f0ad4ea43fff9
monzuralam <monzur707@gmail.com>
2023-03-15 22:33:42 +0600

    PDO prepare statement with question mark placeholder & array()

--- php-oop/43.php
+++ php-oop/43.php
@@ -10,22 +10,33 @@ $pass = '';
 
 try {
     $pdo = new PDO( $dsn, $user, $pass );
-    echo "Database connect successfully.";
+    // echo "Database connect successfully.";
 } catch (\Throwable $th) {
     echo "Database Connection error." . $th->getMessage();
 }
 
 $id = null;
-$name = "MonzurAlam";
+$name = "Alam";
 $position = "WordPress Developer";
-$skill = "WordPress, PHP,mySql, jQuery, Docker, kubernet";
+$skill = "WordPress, PHP,mySql, jQuery";
+
+/*
 $sql = "INSERT INTO users( id, name, position, skill ) Values( :id, :name, :position, :skill )";
 $data = $pdo->prepare( $sql );
 $data->bindParam( ':id', $id );
 $data->bindParam( ':name', $name );
 $data->bindParam( ':position', $position );
 $data->bindParam( ':skill', $skill );
-$data->execute();
+$data->execute(); 
+*/
 
+$sql = "INSERT INTO users( id, name, position, skill ) Values( ?, ?, ?, ? )";
+$data = $pdo->prepare( $sql );
+$arr = array( $id, $name, $position, $skill );
+$query = $data->execute( $arr );
+
+if( $query ){
+    echo "Data has been Inserted successfully.";
+}
 
 include 'inc/footer.php';
