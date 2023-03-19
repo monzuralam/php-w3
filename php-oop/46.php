583701f7729f634aac8897dd4f31e3cc48bca216
monzuralam <monzur707@gmail.com>
2023-03-19 22:41:12 +0600

    Delete data with prepare statement

--- php-oop/46.php
+++ php-oop/46.php
@@ -14,6 +14,7 @@ try {
     echo "Database Connection error." . $th->getMessage();
 }
 
+/* 
 $id = 1;
 $name = 'Ahasan Kabir';
 $postion = 'Web Developer';
@@ -29,6 +30,18 @@ $query = $data->execute();
 
 if( $query ){
     echo "Data has been updated successfully.";
+} else{
+    echo "Something wrong! Please try again.";
+}
+*/
+
+$id = 2;
+$sql = "DELETE FROM users Where id=?";
+$data = $pdo->prepare( $sql );
+$query = $data->execute( array( $id ) );
+
+if( $query ){
+    echo "Data has been Deleted successfully.";
 } else{
     echo "Something wrong! Please try again.";
 }
