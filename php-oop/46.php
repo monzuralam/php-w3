3fbb173f471d744f4b6a359d56ea1d9aaacf8daa
monzuralam <monzur707@gmail.com>
2023-03-19 22:36:09 +0600

    Update data with prepare statement( question mark with bindValue()

---
+++ php-oop/46.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Update data & delele
+ */
+include 'inc/header.php';
+
+$dsn = 'mysql:dbname=php;host=localhost';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO( $dsn, $user, $pass );
+} catch (\PDOException $th) {
+    echo "Database Connection error." . $th->getMessage();
+}
+
+$id = 1;
+$name = 'Ahasan Kabir';
+$postion = 'Web Developer';
+$skill = 'HTML, CSS, JavaScript, jQuery, PHP, MySQL';
+
+$sql = "UPDATE users SET name=?, position=?, skill=? Where id=?";
+$data = $pdo->prepare( $sql );
+$data -> bindValue( 1 , $name );
+$data -> bindValue( 2 , $postion );
+$data -> bindValue( 3, $skill );
+$data -> bindValue( 4 , $id );
+$query = $data->execute();
+
+if( $query ){
+    echo "Data has been updated successfully.";
+} else{
+    echo "Something wrong! Please try again.";
+}
+
+include 'inc/footer.php';
