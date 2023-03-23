288b29f2fc02cbc05c6bb99e27922f511ef34a2e
monzuralam <monzur707@gmail.com>
2023-03-23 07:33:39 +0600

    Another example of Signleton design pattern

--- php-oop/32.php
+++ php-oop/32.php
@@ -9,7 +9,7 @@ Class ProgrammingLanguage{
 
     public function __construct(){
         if( !self::$instance ){
-            self::$instance = new ;
+            self::$instance = $this;
             echo "Created new instance.<br>";
             return self::$instance;
         }else{
@@ -23,5 +23,26 @@ new ProgrammingLanguage();
 new ProgrammingLanguage();
 new ProgrammingLanguage();
 new ProgrammingLanguage();
+
+/**
+ * Real life example of singleton design pattern
+ */
+class Database{
+    public static $database;
+
+    public function __construct(){
+        echo "Database Connection.<br>";
+    }
+
+    public static function connect(){
+        if( ! self::$database ){
+            self::$database = new Database();
+        }
+        return self::$database;
+    }
+}
+
+Database::connect();
+Database::connect();
 
 include 'inc/footer.php';
