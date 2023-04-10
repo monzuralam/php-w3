c9b76388c4585da151f068c95f17235954bbf814
monzuralam <monzur707@gmail.com>
2023-04-10 23:07:38 +0600

    #19 - design home view layout

--- mvc/app/views/home.php
+++ mvc/app/views/home.php
@@ -1,4 +1,88 @@
-<?php
-
-echo "<h2>Home</h2>";
-echo "<p>Load content from views folder</p>";
+<h2>Home</h2>
+<hr>
+<section class="content-section">
+    <div class="container p-0">
+        <div class="row">
+            <div class="col-md-9">
+                <!-- article start here -->
+                <article class="post">
+                    <div class="post-wrapper">
+                        <h2><a href="#">Lorem ipsum dolor sit amet.</a></h2>
+                        <div class="post-meta">
+                            <span>Date: 10/04/2023</span> <span>Author: <a href="#">Monzur Alam</a></span>
+                        </div>
+                        <div class="post-content">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque inventore deleniti at quia. Itaque voluptates placeat eligendi deserunt veritatis repellendus. Beatae quis pariatur veniam modi doloremque nostrum. Velit vitae qui dolores facilis, sint esse perspiciatis. Laborum harum sint quibusdam reiciendis, quaerat modi unde molestiae dolorum cum ex ducimus suscipit.</p>
+                        </div>
+                        <div class="read-more">
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </article>
+                <!-- article stop here -->
+                <!-- article start here -->
+                <article class="post">
+                    <div class="post-wrapper">
+                        <h2><a href="#">Lorem ipsum dolor sit amet elit.</a></h2>
+                        <div class="post-meta">
+                            <span>Date: 10/04/2023</span> <span>Author: <a href="#">Monzur Alam</a></span>
+                        </div>
+                        <div class="post-content">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem qui quam adipisci labore molestiae quasi nobis harum nesciunt ab possimus quis magnam sint ad totam veritatis et tempora in, earum delectus nisi at iusto unde fugit! Impedit, accusantium facilis? Eaque quod ab sed placeat iusto ullam doloribus quo consectetur obcaecati!</p>
+                        </div>
+                        <div class="read-more">
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </article>
+                <!-- article stop here -->
+                <!-- article start here -->
+                <article class="post">
+                    <div class="post-wrapper">
+                        <h2><a href="#">Lorem ipsum dolor sit amet adipsing.</a></h2>
+                        <div class="post-meta">
+                            <span>Date: 10/04/2023</span> <span>Author: <a href="#">Monzur Alam</a></span>
+                        </div>
+                        <div class="post-content">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet culpa explicabo voluptatum dicta autem voluptate nihil pariatur aut iure maxime ea nisi saepe tempore magni dolorem quos fugiat error aspernatur recusandae eius, quidem quasi nam atque eligendi. Voluptates voluptas aliquid fugiat error aliquam cum suscipit, vel a natus tempore. Atque.</p>
+                        </div>
+                        <div class="read-more">
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </article>
+                <!-- article stop here -->
+            </div>
+            <div class="col-md-3">
+                <aside>
+                    <!-- widget start here -->
+                    <div class="widget-item">
+                        <h3>Category</h3>
+                        <div class="widget-wrapper">
+                            <ul>
+                                <li><a href="#">PHP 8</a></li>
+                                <li><a href="#">Laravel</a></li>
+                                <li><a href="#">MVC Framework</a></li>
+                                <li><a href="#">Content Management System</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- widget stop here -->
+                    <!-- widget start here -->
+                    <div class="widget-item">
+                        <h3>Recent Posts</h3>
+                        <div class="widget-wrapper">
+                            <ul>
+                                <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                                <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                                <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                                <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- widget stop here -->
+                </aside>
+            </div>
+        </div>
+    </div>
+</section>
